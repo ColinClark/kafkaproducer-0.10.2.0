54f83ca9b4f6c49884b64ae997142160a076b86e
Colin Clark <colin@clark.ws>
2017-06-20 11:21:31 +0200

    added logging

--- src/main/java/com/clark/kafkaproducer/Main.java
+++ src/main/java/com/clark/kafkaproducer/Main.java
@@ -8,15 +8,22 @@ import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
-
+import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-        // simple approach
+    static {
+        PropertyConfigurator.configureAndWatch("log4j.properties", 5000);
+    }
+
+    public static void main(String[] args) {
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092");
+        // 0, 1, all for acks
         props.put("acks", "0");
         props.put("retries", 0);
         props.put("batch.size", 16384);
@@ -25,16 +32,16 @@ public class Main {
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-        System.out.println("Without Callback");
+        LOGGER.info("Without Callback");
         Producer<String, String> producer = new KafkaProducer<String, String>(props);
         for (int i = 0; i < 100; i++) {
-            System.out.println("Sending message #" + i);
+            LOGGER.info("Sending message #" + i);
             producer.send(new ProducerRecord<String, String>("test", Integer.toString(i), Integer.toString(i)));
         }
         producer.close();
 
         //  with callback
-        System.out.println("With Callback");
+        LOGGER.info("With Callback");
         Producer<String, String> anotherProducer = new KafkaProducer<String, String>(props);
         for (int i = 101; i < 200; i++) {
             ProducerRecord<String, String> myRecord = new ProducerRecord<String, String>("test", Integer.toString(i), Integer.toString(i));
@@ -45,7 +52,7 @@ public class Main {
 
     private static class ProducerCallBack implements org.apache.kafka.clients.producer.Callback {
         public void onCompletion(RecordMetadata metadata, Exception exception) {
-                System.out.println(metadata.topic()+metadata.offset()+metadata.partition());
+                LOGGER.info(metadata.topic()+metadata.offset()+metadata.partition());
         }
     }
 }
